5d56811f43fd8af3b67a076bfbdb019cd5504377
Samet Mavzer <samet.mavzer@intern.priviasecurity.com>
2023-10-08 10:47:00 +0300

    dashboard login page added

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./page.module.css";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Dashboard = () => {
-    return (
-        <div>Dashboard</div>
-    )
-}
 
-export default Dashboard
+    //OLD WAY TO FETCH DATA
+
+    // const [data, setData] = useState([]);
+    // const [err, setErr] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    // useEffect(() => {
+    //   const getData = async () => {
+    //     setIsLoading(true);
+    //     const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    //       cache: "no-store",
+    //     });
+
+    //     if (!res.ok) {
+    //       setErr(true);
+    //     }
+
+    //     const data = await res.json()
+
+    //     setData(data);
+    //     setIsLoading(false);
+    //   };
+    //   getData()
+    // }, []);
+
+    const session = useSession();
+
+    const router = useRouter();
+
+    //NEW WAY TO FETCH DATA
+    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+    const { data, mutate, error, isLoading } = useSWR(
+        `/api/posts?username=${session?.data?.user.name}`,
+        fetcher
+    );
+
+    if (session.status === "loading") {
+        return <p>Loading...</p>;
+    }
+
+    if (session.status === "unauthenticated") {
+        router?.push("/dashboard/login");
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const title = e.target[0].value;
+        const desc = e.target[1].value;
+        const img = e.target[2].value;
+        const content = e.target[3].value;
+
+        try {
+            await fetch("/api/posts", {
+                method: "POST",
+                body: JSON.stringify({
+                    title,
